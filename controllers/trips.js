6459d436e459e4b4dcc3543078860dd3f9e07504
Kee Yu <keeeeeyu@gmail.com>
2022-04-11 14:25:21 -0700

    trip router get/port work

--- controllers/trips.js
+++ controllers/trips.js
@@ -6,18 +6,19 @@ module.exports = {
 }
 
 function newTrip(req, res) {
-    res.render('trips/new')
+    Country.findById(req.params.id, function(err, countries) {
+        res.render('trips/new', {
+            countries
+        })
+    })
 }
 
 function create(req, res) {
     Country.findById(req.params.id, function(err, country) {
-        console.log(req.params.id)
-    //     country.trip.push(req.body);
-    //     console.log(req.body)
-    //     console.log(req.params.id)
-    //     country.save(function(err) {
-    //         if(err) return res.redirect('/trips/new');
-    //     })
-    //     res.redirect(`/countries/${country._id}`)
+        country.trip.push(req.body);
+        country.save(function(err) {
+            if(err) return res.redirect('/trips/new');
+        })
+        res.redirect(`/countries/${country._id}`)
     })
 }
