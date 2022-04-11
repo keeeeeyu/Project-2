bdfdb47bd013f75be5a15f2400598b6d83afb906
Kee Yu <keeeeeyu@gmail.com>
2022-04-11 01:12:14 -0700

    trip schema

--- models/country.js
+++ models/country.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const countrySchema = new Schema({
-    country: {
-        type: String
+const tripSchema = new Schema({
+    city: {type: String},
+    month: {
+        type: String,
+        enum: ['January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August', 'September', 'October',
+        ' November', 'December']
     },
-    // trip: [tripSchema]
+    restaurant: {type: String},
+    Hotel: {type: String},
+    tip: {type: String},
+    like: {type: Number},
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    userName: String,
+    userAvatar: String
+})
+
+const countrySchema = new Schema({
+    country: {type: String},
+    trip: [tripSchema]
 })
 
 
